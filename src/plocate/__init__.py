a85bd1221a07134e716dc7445df1638c06cb0b0f
Dustin Oprea <myselfasunder@gmail.com>
2026-05-25 02:33:40 -0400

    __init__.py: Export DEFAULT_DATABASE_PATH

--- src/plocate/__init__.py
+++ src/plocate/__init__.py
@@ -1,10 +1,12 @@
 """Read and search plocate.db index files."""
 
+import plocate.constants
 import plocate.database
 import plocate.export
 import plocate.search
 
 
+DEFAULT_DATABASE_PATH = plocate.constants.DEFAULT_DATABASE_PATH
 PlocateDatabase = plocate.database.PlocateDatabase
 ExportOptions = plocate.export.ExportOptions
 SearchOptions = plocate.search.SearchOptions
@@ -13,6 +15,7 @@ iter_export_records = plocate.export.iter_export_records
 search_database = plocate.search.search_database
 
 __all__ = [
+    "DEFAULT_DATABASE_PATH",
     "ExportOptions",
     "PlocateDatabase",
     "SearchOptions",
